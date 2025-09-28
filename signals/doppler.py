edc9404c85ba1a4e2c90402113916cba20a732f3
YasmeenBadr <165474689+YasmeenBadr@users.noreply.github.com>
2025-09-28 18:50:51 +0300

    cleaning

--- signals/doppler.py
+++
@@ -1,8 +0,0 @@
-# signals/doppler.py
-from flask import Blueprint, render_template
-
-bp = Blueprint("doppler", __name__, template_folder="../templates")
-
-@bp.route("/")
-def index():
-    return render_template("doppler.html")
--- signals/radar.py
+++
@@ -1,8 +0,0 @@
-# signals/radar.py
-from flask import Blueprint, render_template
-
-bp = Blueprint("radar", __name__, template_folder="../templates")
-
-@bp.route("/")
-def index():
-    return render_template("radar.html")
